dbdf441bce0d2dd888ef89c1ce2ef6daaf80dafd
no <no>
2023-04-20 19:47:02 -0400

    Updated CodebaseEditForGPT4 to mirror relevant command line options from CodebaseEdit.lua

--- CodebaseEditForGPT4.lua
+++ CodebaseEditForGPT4.lua
@@ -1,21 +1,58 @@
-local dkjson = require"dkjson"
 unpack = unpack or table.unpack
+require"ssl.https"
 require"Moonrise.Import.Install".All()
 
-local Filesystem = require"Moonrise.Tools.Filesystem"
+local Tools = require"Moonrise.Tools"
+local GPT_3_5 = require"GPT-3-5"
 
-local Root = arg[1] or error"where?"
-local Extension = arg[2] or error"what?"
-local CommentStart = arg[3] or error"comment starts with?"
+local Options = Tools.CommandLine.GetOptions()
 
-local Files = {}
-for Path, SubPath in Filesystem.Recurse(Root, true) do
-	if SubPath:find(".".. Extension .."$") then
-		local FullPath = Root .."/".. Path .."/".. SubPath
-		local File = io.open(FullPath, "rb")
-		assert(File)
+local Root = Options.Settings.root or "./"
+
+local MinCompletionTokens = tonumber(Options.Settings.min_completion_tokens or 500)
+local MaxTokens = tonumber(Options.Settings.max_tokens or 4097)
+
+local Extensions = Options.Settings.extension or {"lua"}
+Extensions = type(Extensions) == "string" and {Extensions} or Extensions
+local Ignores = Options.Settings.ignore or {"Config.lua$"}
+Ignores = type(Ignores) == "string" and {Ignores} or Ignores
+
+local IncludedNames = {}
+local BaseTokenCount = 0
+local Message = GPT_3_5.API.Message("", "user")
+
+for Path, SubPath in Tools.Filesystem.Recurse(Root, true) do
+	local FullPath = Root .."/".. Path .."/".. SubPath
+
+	local Ignore = true
+	
+	for _,Extension in pairs(Extensions) do
+		if SubPath:match("%.".. Extension .."$") then
+			Ignore = false
+			break
+		end
+	end
+	
+	for _,IgnorePattern in pairs(Ignores) do
+		if FullPath:match(IgnorePattern) then
+			Ignore = true
+			break
+		end
+	end
+	
+	if not Ignore then
+		local File, Err = io.open(FullPath, "rb")
+		assert(File, Err)
 		local Content = File:read"a"
-		table.insert(Files, CommentStart .. FullPath .." follows this line:\n".. Content)
+		local _Message = GPT_3_5.API.Message(Message.content .."\n--[[".. FullPath .." follows this line:]]\n".. Content, "user")
+		local TokenCount = BaseTokenCount + GPT_3_5.API.CountMessageTokens(_Message)
+		if MaxTokens-TokenCount >= MinCompletionTokens then
+			Message = _Message
+			table.insert(IncludedNames, FullPath)
+		else
+		end
+	else
 	end
 end
-print(table.concat(Files, "\n"))
+print(Message.content)
+
